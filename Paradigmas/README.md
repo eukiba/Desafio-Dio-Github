c606b03d2a8637622124a0323919c5b6e5ead875
Renato Nuno <80795209+eukiba@users.noreply.github.com>
2021-08-03 23:56:06 -0300

    Add files via upload

---
+++ Paradigmas/README.md
@@ -0,0 +1,13 @@
+# Olá🖐️
+
+Aqui vemos um exercício mais elaborado em Java sobre Paradigmas_dio
+
+onde tratamos os  principais termos sobre Paradigmas
+
+- Imperativo
+- Funcional
+- Composição de funções
+- Imutabilidade
+- Lambda
+- Interfaces e alguns outros assuntos 
+
---
+++ Paradigmas/src/com/Digital/Innovation/One/Imperativo.java
@@ -0,0 +1,10 @@
+package com.Digital.Innovation.One;
+
+public class Imperativo {
+
+    public static void main(String[] args) {
+        int valor = 10; //instrução
+        int resultado = valor * 3; //instrução
+        System.out.println("O resultado é :: " + resultado); //instrução
+    }
+}
